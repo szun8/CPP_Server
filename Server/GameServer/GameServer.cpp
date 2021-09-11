6dcdd90eec1da64524112f90238ad22ae4378239
tldmschl81 <chse0810@gmail.com>
2021-09-11 15:04:28 +0900

    1. Lock 기초

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,57 +1,75 @@
 ﻿#include "pch.h"
 #include "CorePch.h"
 #include <iostream>
-// windows / Linux 환경에서 공용으로 스레드 생성 가능한 헤더
+// windows / Linux 환경에서 공용으로 스레드 활용 가능한 헤더
 #include <thread> 
 #include <atomic>
+#include <mutex>	// Lock
 
-// part4 ) 1-3. Atomic (All-Or-Nothing)
-// atom : 원자
-// 멀티쓰레드에서 stack은 각기 다른 영역으로 데이터를 차지하고 있음
-// 해당 연산을 쓰면, 한 연산에서 atomic변수를 건드릴 때 다른 연산이 지금 연산에 침범하지 않도록 cpu 내에서 막아버리는? 기다리게하는 과정을 처리함
+// part4 ) 1-4. Lock
+// 공유데이터의 동시접근 [X]
+// Mutual Exclusive (상호배타적)
+// 사용하는 범위 조심
 
-atomic<int32> sum = 0;	// 전역(Heap), 공유데이터
+vector<int32> v;
+// 기본적으로 자료구조 컨테이너는 멀티쓰레드에서 동작하지 않는다고 가정
 
-void Add() {
-	for (int32 i = 0; i < 100'0000; i++) {
-		sum.fetch_add(1);
-		// ++sum;
-		// 어셈블리어(3줄?) = cpu에서 메모리를 꺼내오고 연산하는 것을 동시에 할 수 없음
-		// int32 eax = sum;		// 1. eax = 0
-		// eax = eax + 1;		// 2. eax = 1
-		// sum = eax;			// 3. sum = 1
+// // [crash가 나는 이유]
+// 동적배열 특성상 capacity가 꽉차면 새로 늘려주거나 새로운 메모리 할당 후
+// 기존 메모리 복사 - 삭제의 과정을 거침
+// 그런데 멀티쓰레드에서 이 과정을 거친다면, 똑같은 과정을 쓰레드수만큼 불규칙적이게 실행(할당-복사-삭제)함
+// double-free 현상이 발생
+
+// 그렇다면 충분히 메모리를 우선 할당(reserve)후 push를 해준다면 해결되지 않을까?
+// 불규칙하게 push를 실행하기 때문에 동일 메모리에 중복 접근 실행가능 => 조금의 메모리 손실(원한만큼의 size가 도출이 안되는 현상)
+
+// 규칙적으로 원활히 코드 실행을 할 수는 없을까?
+// 일단 atomic은 사용 불가 (container의 기능은 못쓰고 atomic의 기능만 사용가능) 
+// (!) Lock 사용 : 한 과정을 하는 동안 다른 쓰레드는 해당 데이터에 침입이 불가하게 [Lock]을 걸어주는 규칙을 만들어주는 것
+
+mutex m;	// 일종의 자물쇠, 재귀적(이중적) 호출 불가 => 다른 버전 사용, unlock()을 안해주면 안됨,,,
+
+// RAII (Resource Acquisition Is Initialization) : 하나하나 Lock/unLock을 해주는 불편을 해결
+template<typename T>
+class LockGuard {	// [Lock 자동문]
+public:
+	LockGuard(T& m) {
+		_mutex = &m;
+		_mutex->lock();
+	}
+	
+	~LockGuard(T& m) {
+		_mutex->unlock();
+	}
+
+private:
+	T* _mutex;
+};
+void Push() {
+	for (int32 i = 0; i < 10000; ++i) {
+		// m.lock();			// 잠기
+		
+		// LockGuard<std::mutex> lockGuard(m);	= 객체의 유효범위가 끝나면 break를 했던 뭐든 소멸이되기에 unlock을 자동으로 실행시켜줌
+		std::lock_guard<std::mutex> lockGuard(m);	// std 표준도 존재
+		std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock);// lockGuard랑 기능은 동일, 추가적으로 옵션지정 가능
+		//defer_lock : 일단 lock에 대한 interface만 제공하고(바로 잠그지 않고) 명시적으로 lock을 걸어주면 그때 잠기는 옵션
+		uniqueLock.lock();		// 잠기는 시점 지정
+
+		v.push_back(i);
+		if (i == 5000) break;	// unlock을 하지 않고 해당 쓰레드가 잠기는 현상 발생 => class LockGuard 해결
+
+		// m.unlock();			// 풀기 => 싱글쓰레드처럼 동작(느릴수있음)
 	}
 }
-
-void Sub() {
-	for (int32 i = 0; i < 100'0000; i++) {
-		sum.fetch_add(-1);
-		// --sum;
-		// int32 eax = sum;		// 1. eax = 0
-		// eax = eax - 1;		// 2. eax = -1
-		// sum = eax;			// 3. sum = -1
-	}
-}
-// 멀티쓰레드 환경에서 두개를 동시에 실행한다면, 어떤 것이 먼저실행되고 나중에 실행될진 모르지만,
-// 섞여서 실행 후, 값이 덮어쓰여서 값의 충돌로 오류가 발생 => if 1st tried, sum = 1 or -1
-// 공유데이터의 단점 (수정시)
-
-// 동기화 : 공유데이터를 다룰때 순서를 정해줘야하는 경우
-// 애초에 1~3번의 단계를 한번에 실행해주면 값의 충돌현상을 없앨 수 있음 (오류제거) = 동기화 기법 사용
-// => Atomic 연산 : All-Or-Nothing 다 실행이 되거나 아예 안하거나 둘중 하나의 상황만 존재할 수 있는 경우의 연산, 연산이 근데 느리김함(필요한 경우만, 병목현상 발생가능)
 
 int main()
 {
-	Add();
-	Sub();
-	cout << sum << endl;	// 0
+	std::thread t1(Push);
+	std::thread t2(Push);
 
-	std::thread t1(Add);
-	std::thread t2(Sub);
 	t1.join();
 	t2.join();
 
-	cout << sum << endl;	// (?) 엉뚱한 숫자 -> atomic -> 0 (!)
-
+	cout << v.size() << endl;
 	return 0;
 }
