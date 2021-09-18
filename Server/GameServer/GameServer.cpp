8ab510211fb751e395c4597059aa8df443ca3d5c
tldmschl81 <chse0810@gmail.com>
2021-09-18 16:29:07 +0900

    1. Condition Variable

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,48 +7,63 @@
 #include <mutex>	// Lock
 #include <windows.h>
 
-// part4 ) 1-9 Event
+// part4 ) 1-10 Condition Variable
+// Event의 연장선, 조건변수
 	
 mutex m;
 queue<int32> q;
 HANDLE handle;	// handle : 어떤 Event인지 구분해주는 식별자
 
+condition_variable cv;
+
 void Producer() {
 	while (true) {
+		
+		// 1) Lock을 잡고
+		// 2) 공유변수 값을 수정
+		// 3) Lock을 풀고
+		// 4) 조건변수 통해 다른 쓰레드에게 통지
+		
+		// 3번을 안하고 4번을 해도되냐? NO!
+		// 쓰레드를 깨워도 먼저하는 것이 lock이기에 여기서 lock을 안풀어주면
+		// 깨워진 쓰레드에서 lock을 잡지 못함
+
 		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
+			unique_lock<mutex> lock(m);	// 1, 3번
+			q.push(100);				// 2번
 		}
-		::SetEvent(handle);	// 데이터가 넣어지면, 해당 커널오브젝트를 파란불로 바꿔주세요 -> Consumer 실행!
-		this_thread::sleep_for(1000000ms);	// 느리게 데이터 삽입
+		cv.notify_one(); // 4번, Wait중인 쓰레드가 있으면 딱 1개를 깨운다
+		//::SetEvent(handle);	// 데이터가 넣어지면, 해당 커널오브젝트를 파란불로 바꿔주세요 -> Consumer 실행!
+		//this_thread::sleep_for(1000000ms);	// 느리게 데이터 삽입
 	}
 }
 
 void Consumer() {
 	while (true) {
-		::WaitForSingleObject(handle, INFINITE);
-		// 파란불이면 아래 코드 실행 / 빨간불이면 수면상태(대기, 아래 코드 실행 X), CPU점유율 절감
-		// Auto-Reset(false)에서는 파란불이어서 아래 코드가 실행되면 다시 시그널이 Non-Signal로 바뀐다(자동)
-		::ResetEvent(handle);	// Manual-Reset(true)은 수동으로 코드 쳐줘야함
-
-		// Producer에서 data를 안넣어주면 Consumer는 할필요가 없음 (잉여작업)
+		
 		unique_lock<mutex> lock(m);
-		if (q.empty() == false) {
+		cv.wait(lock, []() {return q.empty() == false; });
+		// 1) Lock을 잡으려고 시도 (이미 잡혔으면 통과)
+		// 2) 조건확인
+		//	- 만족 0 => 빠져나와서 이어서 코드를 진행
+		
+		// 그런데 notify_one 을 했으ㅡ면 항상 조건식을 만족하는거 아닐까?
+		// Spurious Wakeup (가짜 기상?)
+		// notify_one할 때 lock을 잡고 있는 것이 아니기 때문에 크로스체킹
+		// 조건과 notify는 독립적으로 시행하기에 충분히 조건을 걸어줘야함
+		// if (q.empty() == false) - 위에서 람다로 조건을 이미 통과시켜준 다음이므로 없어도됨
+		{
 			int32 data = q.front();
 			q.pop();
 			cout << data << endl;
 		}
+
+		//  - 만족 X => Lock을 풀어주고(unique_lock) 대기 상태 전환
 	}
 }
 
 int main()
 {
-	// kernel Object (커널에서 관리/사용하는 오브젝트) -> 다른 프로그램 동기화 작업시 사용
-	// Usage Count 이 obj를 몇명이 사용중인지
-	// Signal(파란불, true) / Non-Signal(빨간불, false) << BOOL
-	// Auto / Manual << BOOL
-	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*MANUAL_RESET*/, FALSE/*binitialState*/,NULL );
-	
 	thread t1(Producer);
 	thread t2(Consumer);
 
@@ -56,6 +71,4 @@ int main()
 	t2.join();
 
 	return 0;
-
-	::CloseHandle(handle);	// 생성 소멸 짝꿍의 느낌
 }
